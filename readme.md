e26bd7715c01d55afa0551a07bd7d0a5efbe962d
External Time <84183548+ExternalTime@users.noreply.github.com>
2023-04-28 20:49:20 +0200

    actual initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Ngram typing speed test
+
+This is a very simple app for measuring typing speed of different
+key sequences. It does not support persistance or anything else
+you might like to have.
+
+Custom tests can be specified as javascript code that evaluates to
+an array of strings.
---
+++ script.js
@@ -0,0 +1,78 @@
+"use strict";
+
+let status = document.getElementById("status");
+let last_keypress = document.getElementById("last_keypress");
+let last_time = document.getElementById("last_time");
+
+let times = new Map();
+let ngrams = [];
+let time = [];
+let max_bigram_speed = 400;
+
+let target = "";
+
+document.addEventListener("keydown", function(e) {
+  if (e.key == "/" || e.key == "'")
+    e.preventDefault();
+  last_keypress.innerText = "last pressed key: " + e.key;
+  if (target.charAt(time.length) !== e.key)
+    return;
+  time.push(e.timeStamp);
+  if (time.length != target.length)
+    return;
+  let t0 = time.shift();
+  let res = time.map(x => x - t0);
+  time.length = 0;
+  if (max_bigram_speed * (target.length - 1) < time.at(-1))
+    return;
+  if (!times.has(target))
+    times.set(target, []);
+  times.get(target).push(res);
+  last_time.innerText = "last time: " + res.at(-1);
+  next_ngram();
+});
+
+function next_ngram() {
+  target = ngrams.pop();
+  if (target === undefined) {
+    target = "";
+    status.innerText = "Done!";
+    return;
+  }
+  status.innerText = "next word: " + target;
+}
+
+function import_test() {
+  let imported = prompt("paste the testing plan here");
+  if (imported === null) return;
+  ngrams = eval?.(`"use strict";(${imported})`);
+  next_ngram();
+}
+
+function export_data() {
+  prompt(undefined, JSON.stringify(Array.from(times.entries())))
+}
+
+function* alphabet() {
+    let x = 'a'.charCodeAt(0);
+    do {
+      yield String.fromCharCode(x);
+    } while (x++ < 'z'.charCodeAt(0));
+}
+
+function bigrams() {
+  let chars = [
+    ...alphabet(),
+    ';',
+    ',',
+    '.',
+    '/',
+  ];
+  let bigrams = chars.flatMap(x => chars.map(y => x + y));
+  for (let i = bigrams.length - 1; 0 < i; i--) {
+    let j = Math.floor(Math.random() * i);
+    [bigrams[i], bigrams[j]] = [bigrams[j], bigrams[i]];
+  }
+  ngrams = bigrams.flatMap(bigram => Array(4).fill(bigram));
+  next_ngram();
+}
